d917ae0aa178c417596e41bfb08d1215d5fd2fb1
davila <davilaketilly59@gmail.com>
2025-04-03 07:56:41 -0300

    eviando exercicio 02

--- exercicio 01.py
+++ exercicio 01.py
@@ -1,5 +1,14 @@
 nome = input ("digite seu nome: ")
 idade = int(input("digite sua idade: "))
 salario = float(input("digite seu salario: "))
+percentual =float(input("digite seu percentual: "))
 
-print (f"seu nome é {nome}, sua idade é {idade}, seu salario é {salario}")
+porcentagem = (salario*percentual)
+salarioatual = (percentual+salario)
+
+print(f"olá {nome}, sua idade é {idade} anos, voce recebe {salario:.2f}, teve aumento de R${porcentagem:.2f} e seu salario atual é R${salarioatual:.2f}. ")
+
+
+
+
+
---
+++ exercicio 02.py
@@ -0,0 +1,8 @@
+N1 = float(input(" digite seu numero: "))
+N2 = float(input("digite seu numero: "))
+adicao= N1+N2
+subritracao = N1-N2
+divisao =N1/N2
+mutiplicacao = N1*N2
+
+print f""
