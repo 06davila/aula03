ab0f7624194bd83d26386f8592bed0fc50f97e71
davila <davilaketilly59@gmail.com>
2025-04-03 07:58:44 -0300

    eviando exercicio 04

---
+++ exemplo4.py
@@ -0,0 +1,10 @@
+n1 = input ("digite seu numero")
+n2 = input (" digite seu numero")
+
+if n1>n2:
+    print (n1,n2)
+if media >=7:
+print (f" aluno aprovado com media {media}")
+else: media<4:
+    print (f " aluno reprovado com media {media}")
+else: ptint (f"aluno "
